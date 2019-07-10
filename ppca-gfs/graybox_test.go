0885cbc421ea09e15ca9421b87d4f467f4c69b99
chaomai <loneymai@gmail.com>
2019-07-10 18:07:22 +0800

    update

--- ppca-gfs/graybox_test.go
+++ ppca-gfs/graybox_test.go
@@ -842,13 +842,13 @@ func TestPersistentMaster(t *testing.T) {
 	time.Sleep(2 * gfs.ServerTimeout)
 
 	// restart
-	// m = master.NewAndServe(mAdd, path.Join(root, "m"))
-	// time.Sleep(2 * gfs.ServerTimeout)
-	//
-	// // check recovery
-	// checkWork(p, msg, t)
-	//
-	// errorAll(ch, 3, t)
+	m = master.NewAndServe(mAdd, path.Join(root, "m"))
+	time.Sleep(2 * gfs.ServerTimeout)
+
+	// check recovery
+	checkWork(p, msg, t)
+
+	errorAll(ch, 3, t)
 }
 
 // delete all files in two chunkservers ...
